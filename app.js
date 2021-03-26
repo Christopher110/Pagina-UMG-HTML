3d26c800048100eba3c5b74ecc3034143dea3c4d
Christopher110 <81401792+Christopher110@users.noreply.github.com>
2021-03-25 22:05:00 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const texto1 = document.getElementById("texto1");
+const texto2 = document.getElementById("texto2");
+const texto3 = document.getElementById("texto3");
+const texto4 = document.getElementById("texto4");
+
+function mostrarPersonales (){
+    texto1.innerHTML="Christopher Alejandro Ramos"
+    texto2.innerHTML="Edad: 19 años"
+    texto3.innerHTML="Sexo: Masculino"
+    texto4.innerHTML="Carné No. 9989-21-2587"
+}
+
+function mostrarColegio (){
+    texto1.innerHTML="Instituto Nacional de Bachillerato en Computación"
+    texto2.innerHTML="Se encuentra ubicado en la Zona 1"
+    texto3.innerHTML="El mismo lugar, es el establecimiento del Instituto PEMEM"
+    texto4.innerHTML="Horario: 1PM -5:45PM"
+}
+
+function mostrarUniversidad (){
+    texto1.innerHTML="Universidad Mariano Gálvez de Guatemala"
+    texto2.innerHTML="Zona Portales, Guatemala, 12 calle 4-10 zona 17 Carretera al Atlánticoe"
+    texto3.innerHTML="Plan Sabado - Horario: 07:00 AM - 18:00 PM"
+    texto4.innerHTML="PBX: 2235 1383  /  portales@umg.edu.gt"
+}
+
+function mostrarHobbie (){
+    texto1.innerHTML="Tocar Piano"
+    texto2.innerHTML="Me gusta cocinar"
+    texto3.innerHTML="Escuchar y hacer música"
+    texto4.innerHTML="Hago ejercicio"
+}
